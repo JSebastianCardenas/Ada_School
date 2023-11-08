4f16b5fd17b0e9187ed0f05884f008808ca3dab8
Sebastian Cardenas <juan-zxc@hotmail.com>
2023-11-08 09:52:16 -0500

    Segunda subida

--- uso_git.py
+++ uso_git.py
@@ -3,6 +3,7 @@ git init: Inicia un nuevo espacio de trabajo donde puedes guardar y hacer un seg
 git clone <URL>: Descarga una copia completa de un proyecto desde Internet para que puedas trabajar en él en tu propia computadora.
 git status: Te muestra una lista de los cambios que has realizado en tus archivos y te dice si están listos para ser guardados.
 git add <nombre-archivo>: Marca los cambios específicos que deseas guardar en tu proyecto. Es como decirle a Git, "hey, estos cambios son importantes".
+git log: Muestra los commits realizados
 git commit -m "Mensaje de confirmación": Guarda los cambios que seleccionaste anteriormente y le das un nombre para que puedas recordar qué hiciste.
 git diff: Muestra las diferencias entre los cambios que hiciste en tus archivos y cómo eran antes.
 git branch <nombre-rama>: Crea un espacio separado donde puedes trabajar en cambios sin afectar el trabajo principal.
